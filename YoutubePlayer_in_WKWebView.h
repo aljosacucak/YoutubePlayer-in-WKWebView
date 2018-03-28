f1d06a53a2da1e94d28f9c632fd65e9d87f1492f
Aljosa Cucak <aljosacucak@gmail.com>
2018-03-28 13:06:21 -0500

    bundled into a framework

---
+++ YoutubePlayer_in_WKWebView.h
@@ -0,0 +1,19 @@
+//
+//  YoutubePlayer_in_WKWebView.h
+//  YoutubePlayer-in-WKWebView
+//
+//  Created by Aljosa Cucak on 3/28/18.
+//  Copyright © 2018 Aljosa Cucak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YoutubePlayer_in_WKWebView.
+FOUNDATION_EXPORT double YoutubePlayer_in_WKWebViewVersionNumber;
+
+//! Project version string for YoutubePlayer_in_WKWebView.
+FOUNDATION_EXPORT const unsigned char YoutubePlayer_in_WKWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YoutubePlayer_in_WKWebView/PublicHeader.h>
+
+#include "WKYTPlayerView.h"
